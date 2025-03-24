48623b46b51757ea9f9463d8af8dff2e38ed03a7
jeff <jeffrey-31@hotmail.fr>
2025-03-24 20:40:19 +0100

    add function to print a string and a percent

--- function.c
+++ function.c
@@ -7,7 +7,41 @@
  *return: no return
 */
 
-void printchar(va_list args)
+int printchar(va_list args)
 {
-	_putchar(args);
+	int c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
+
+/**
+ *
+ *
+ *
+*/
+
+int printstring(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int lenght = 0;
+
+	while (*str)
+	{
+		_putchar(*str++);
+		lenght++;
+	}
+	return (lenght);
+}
+
+/**
+ *printpercent - print % if %% in _printf
+ *return: 1
+ */
+
+int printpercent()
+{
+	_putchar('%');
+	return (1);
 }
