ee762aa3581c014eb69e90e55ef752ba8477cab1
jeff <jeffrey-31@hotmail.fr>
2025-03-24 15:38:11 +0100

    creating file function and main test

---
+++ function.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ *printchar - function who's print only one char
+ *@args: Keep arg to print
+ *return: no return
+*/
+
+void printchar(va_list args)
+{
+	_putchar(args);
+}
