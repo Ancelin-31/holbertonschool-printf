1293e54afbfb130254b0d9f58546e5c194b5f99d
jeff <jeffrey-31@hotmail.fr>
2025-03-25 10:36:22 +0100

    to fix merge problems

--- main.c
+++ main.c
@@ -9,13 +9,22 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Let's try to printf a simple sentence.\n");
+    printf("Let's try to printf a simple sentence.\n");
+    
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
 
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+
+    _printf("Percent:[%%]\n");
+    printf("Percent:[%%]\n");
+
+    _printf("Length:[%d, %i]\n", 40, 40);
+    printf("Length:[%d, %i]\n", 39, 39);
+
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
     return (0);
 }
