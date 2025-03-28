6dca6d4f934c54984ef6b9992f5392bc65683672
jeff <jeffrey-31@hotmail.fr>
2025-03-28 15:09:01 +0100

    to checkout ouput in man exemple

--- main.c
+++ main.c
@@ -9,15 +9,23 @@
  */
 int main(void)
 {
-	int age = 25;
-	char name[] = "Alice";
+	char name[] = "Ancelin";
+	int age = 31;
 
-	_printf("%c%c ! My name is %s, i'm %d years\n", 'H', 'i', name, age);
-	
+	/* Exemple to print a string */
+	_printf("Hello ! We are %s and %s.\n", name, "Jeffrey");
+
+	/* Exemple to print a number */
+	_printf("We are respectively %d and %i.\n", age, 37);
+
+	/* Exemple to print a character and % */
+	_printf("We are %c%c-developer and we are at 200%%\n", 'C', 'o');
 	return (0);
 }
 
 /**
  * Output:
- * Hi ! My name is Alice, i'm 25 years
+ * Hello ! We are Ancelin and Jeffrey.
+ * We are respectively 31 and 37.
+ * We are Co-developer and we are at 200%.
 */
