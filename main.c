f869b9c3f6bcf5b76c70bc998333414da9f0c0d8
jeff <jeffrey-31@hotmail.fr>
2025-03-28 14:01:56 +0100

    exemple to readme

--- main.c
+++ main.c
@@ -9,23 +9,15 @@
  */
 int main(void)
 {
-	/* test %c %s %% and observe if there is a correct output */
-	 _printf("%c", 'S');
-	 _printf("%c", '\0');
-	 printf("css%ccs%scscscs", 'T', "Test");
-	 printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
-	 _printf("%%");
-	 _printf("%!\n");
+	int age = 25;
+	char name[] = "Alice";
 
-	 /* test %d %i */
-	 _printf("%d == %i\n", 1024, 1024);
-	 _printf("iddi%diddiiddi\n", 1024);
-	 _printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-
-	 /* test %b %o %u %x %X*/
-
-	 _printf("%b", UINT_MAX);
-	 _printf("%u == %o == %x == %X\n", 1024, 1024, 1024, 1024);
-	 
-    return (0);
+	_printf("%c%c ! My name is %s, i'm %d years\n", 'H', 'i', name, age);
+	
+	return (0);
 }
+
+/**
+ * Output:
+ * Hi ! My name is Alice, i'm 25 years
+*/
